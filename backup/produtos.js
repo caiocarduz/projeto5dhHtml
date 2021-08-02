fde82d463f5553531fcf82d79016d52d9842a216
caio <caio.carduz@gmail.com>
2021-08-02 12:02:58 -0300

    adicionada gitignore e html da pagina produtos

---
+++ backup/produtos.js
@@ -0,0 +1,11 @@
+// external js: flickity.pkgd.js
+
+// card
+
+$('.buy').click(function(){
+    $('.bottom').addClass("clicked");
+  });
+  
+  $('.remove').click(function(){
+    $('.bottom').removeClass("clicked");
+  });
