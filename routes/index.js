49366769608c9aa4af76fe5f0458e245218637bd
Romsgs <rsalobrena@gmail.com>
2021-08-11 22:54:46 -0300

    subindo minha header

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var indexController = require('../controllers/indexController')
+var carrinhoController = require('../controllers/carrinhoController')
+
 
 /* GET home page. */
 router.get('/', indexController.home);
 router.get('/produtos', indexController.produtos)
 router.get('/login', indexController.login)
-
+router.get('/carrinho', carrinhoController.carrinho);
 module.exports = router;
