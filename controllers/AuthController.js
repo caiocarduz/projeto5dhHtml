b4e07388ba5ef139336bed100ca221d53be5b7ca
Romsgs <rsalobrena@gmail.com>
2021-08-26 14:08:34 -0300

    create User funciona

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -10,10 +10,19 @@ module.exports = {
         console.log('entrou na requisicao')
         const { email, password } = req.body;
         console.log( email, password)
-        const user = usuarios.find( user => user.email == email && user.password == password)
-        
-        if(user){
-            req.session.user = {nome: user.nome, email: user.email}
+        try {
+            validationResult(req).throw();
+            // Oh look at ma' success! All validations passed!
+        } catch (err) {
+            console.log(err.mapped()); // Oh noes!
+            const erros = validationResult(req);
+            //se tiver erros manda de volta o erro
+            res.send(erros)
+        }
+        const usuario = usuarios.find( usuario => usuario.email == email && usuario.password == password)
+        console.log(usuario)
+        if(usuario != undefined){
+            req.session.usuario = {nome: usuario.nome, email: usuario.email}
             res.send('<h1>logado</h1>')
         } else {
             res.send('user nao existe no banco de dados')
@@ -23,13 +32,16 @@ module.exports = {
 
     },
     createUser:  (req, res) => {
+        
+    
+        const erros = validationResult(req);
         const { email, password } = req.body;
         console.log(password)
         console.log(email)
         const user = usuarios.find( user => user.email == email && user.password == password)
         
         if(user){
-            res.send( `este email ja existe favor fazer login UMA IDEIA é REDIRECIONAR PARA UMA PAGINA ONDE SO TEM O CAMPO DE LOGIN`);
+            res.render('loginJaExiste', {erros});
         }else {
             let novoUsuario = {
                 nome:email,
@@ -43,7 +55,7 @@ module.exports = {
             // fs.writeFileSync(path.join(__dirname, "../database/usuarios.json"), JSON.stringify(novoUsuario, null, 1));
              fs.writeFileSync(path.join(__dirname, "../database/usuarios.json"), JSON.stringify(usuarios,null,1));
              console.log(user)
-             res.send(usuarios)
+             res.render('/', {menssagem:"Usuario criado corretamente"})
             }
 
         
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { body, validationResult } = require('express-validator')
 const AuthController = require('../controllers/AuthController')
 
-router.post('/logUser', body('password').isLength({ min: 5 }),body('email').isEmail(), AuthController.logUser)
+router.post('/logUser', body('password').notEmpty(),body('email').isEmail(), AuthController.logUser)
 router.post('/createUser', body('password').notEmpty(), body('email').isEmail(), AuthController.createUser)
 
 module.exports = router
