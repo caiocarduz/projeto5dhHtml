ba7b30f8d2a1fc1f3986bea232b523a5f5a4e3db
caiocarduz <caio.carduz@gmail.com>
2021-10-11 14:21:44 -0300

    inserido migration e modelo de produtos

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,8 +2,11 @@
 const cards1 = require('../dataBase/cardsHome.json')
 const cards2 = require('../dataBase/cards2Home.json')
 const detalhes = require('../dataBase/produtoDetalhes.json')
+const {Produto, sequelize} = require("../models");
 module.exports = {
-    home: (req, res) =>{
+    home: async (req, res) =>{
+        const cards2 = await Produto.findAll();
+        console.log(cards2)
         res.render('home', {cards1, cards2, logged_user : req.session.user});
         
     },
