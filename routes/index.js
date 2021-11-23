8501a2e163bb105fdbaa4073c7457efbc263fd31
Romsgs <rsalobrena@gmail.com>
2021-11-23 00:21:36 -0300

    adcionando funcionalidade nas categorias mas com problemas ainda

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/', indexController.home);
 router.get('/index', indexController.home);
 router.get('/home', indexController.home);
 router.get('/produtos', indexController.produtos)
+router.get('/produtos/categoria/:idCategoria', indexController.getCategoria)
 router.get('/login', indexController.login)
 router.get('/carrinho', carrinhoController.carrinho);
 router.get('/produtodetalhes/:id', indexController.produtoDetalhes)
